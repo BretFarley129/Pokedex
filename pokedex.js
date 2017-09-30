2c3be3b19704e74c9e053022c45a4fb967b468ff
Bret Farley <bretfarley12@mittymonarchl.com>
2017-09-30 15:12:01 -0700

    added loading message

--- pokedex.js
+++ pokedex.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
 
     
-    
+    //Puts all images on load
     for(var i=1; i<= 151; i ++){
         $('#pokemon').append(`<img src="http://pokeapi.co/media/img/${i}.png" num="${i}">`);
     }
+
+    //puts information in pokedex sidebar
     $(document).on("click", 'img', function(){
+        console.log("1");
         var no = $(this).attr('num');
+        console.log("2");
+
+        //loading text
+        $('#dex').html(
+            "<p>Loading...</p>"
+        );
+
         $.get("https://pokeapi.co/api/v2/pokemon/"+no+"/", function(res) {
-            // console.log(res);
+            console.log("3");
             var typehtml = "<ul>";
             for(var i  = 0; i< res.types.length; i++){
                 typehtml +="<li>"+res.types[i].type.name+"</li>";
             }
+            console.log("4");
             typehtml += "</ul>"
             console.log('dont ban me please');
             console.log(res.name);
@@ -27,18 +38,18 @@ $(document).ready(function(){
                 <h4>Weight</h4>
                 <p>${res.weight}</p>
                 `
-        );}, "json");
+            );}, "json");
     });
     
-    $('img').click(()=>{
-        var temp = $(this).attr("num");
-        console.log(temp);
-        var pic = 'http://pokeapi.co/media/img/'+temp+'.png';
-        $('#dex img').attr('src', pic);
-    });
-    $(document).on("click", "img", function(){
-        var pic = $(this).attr('src');
-        $('#dex img').attr('src', pic);
-    });
+    // $('img').click(()=>{
+    //     var temp = $(this).attr("num");
+    //     console.log(temp);
+    //     var pic = 'http://pokeapi.co/media/img/'+temp+'.png';
+    //     $('#dex img').attr('src', pic);
+    // });
+    // $(document).on("click", "img", function(){
+    //     var pic = $(this).attr('src');
+    //     $('#dex img').attr('src', pic);
+    // });
 
 });
